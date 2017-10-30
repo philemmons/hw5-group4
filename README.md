908089787ee8b8f3669cd2dba916ac97bd5bc794
pemmons <pemmons@csumb.edu>
2017-10-30 00:00:17 +0000

    init git, add code, etc.

---
+++ README.md
@@ -0,0 +1,32 @@
+
+     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
+    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
+    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
+    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
+     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
+    ----------------------------------------------------------------- 
+
+
+Welcome to your Node.js project on Cloud9 IDE!
+
+This chat example showcases how to use `socket.io` with a static `express` server.
+
+## Running the server
+
+1) Open `server.js` and start the app by clicking on the "Run" button in the top menu.
+
+2) Alternatively you can launch the app from the Terminal:
+
+    $ node server.js
+
+Once the server is running, open the project in the shape of 'https://projectname-username.c9users.io/'. As you enter your name, watch the Users list (on the left) update. Once you press Enter or Send, the message is shared with all connected clients.
+
+// https://glebbahmutov.com/blog/angularjs-inside-jade-templates/
+// https://stackoverflow.com/questions/31225448/angular-data-binding-not-working-with-jade
+// http://www.tutorialsteacher.com/nodejs/jade-template-engine
+// http://www.anujgakhar.com/2013/06/15/duplicates-in-a-repeater-are-not-allowed-in-angularjs/
+// https://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
+// https://stackoverflow.com/questions/14167743/why-cant-jade-find-my-resources
+// **** Mostafa Eweda <mo.eweda@gmail.com> - as a base template for the chat from cloud9
+// https://stackoverflow.com/questions/23989463/how-to-set-tbody-height-with-overflow-scroll
+// 
---
+++ server.js
@@ -0,0 +1,84 @@
+
+let http = require('http');
+let path = require('path');
+let async = require('async');
+let socketio = require('socket.io');
+let express = require('express');
+
+let router = express();
+let server = http.createServer(router);
+let io = socketio.listen(server);
+
+router.use(express.static(__dirname + '/client'));
+router.set('views', path.join(__dirname, 'client/views'));
+
+router.set('view engine','jade');
+//router.configure('development', function( ) { router.locals.pretty = true; });
+router.get('/', function( req, res ){
+  res.render( 'index' );
+});
+
+let socketArr = [];
+let userTextArr = [];
+let numUser = 0;
+
+io.on('connection', function(socket){
+  numUser++;
+    userTextArr.forEach(function(data){
+      socket.emit('message', data);
+    });
+
+    socketArr.push(socket);
+
+    socket.on('message', function(textInput){
+      let userText = String(textInput || '');
+
+      if (!userText)
+        return;
+
+      socket.get('name', function(err, userName){
+        let data = { name: userName,
+                     text: userText
+        };
+        allClients('message', data);
+        userTextArr.push(data);
+      });
+    });
+
+    socket.on('identify', function(name){
+      socket.set('name', String(name || 'Anonymous'), function(err){
+        updateUserList();
+      });
+    });
+    
+    
+    socket.on('disconnect', function(){
+      numUser--;
+      socketArr.splice(socketArr.indexOf(socket), 1);
+      updateUserList();
+    });
+
+  });
+
+function allClients( event, eventData){
+  socketArr.forEach( function (socket){
+    socket.emit(event, eventData);
+  });
+}
+
+function updateUserList(){
+  async.map( socketArr,
+            function( socket, callback){
+              socket.get( 'name', callback);
+            },
+            function( err, names){
+              allClients( 'userList', names);
+            }
+  );
+  allClients( 'numUser', numUser );
+}
+
+server.listen(process.env.PORT || 3000, process.env.IP || "127.0.0.1", function(){
+  let addr = server.address();
+  console.log("server listening:", addr.address + ":" + addr.port);
+});
